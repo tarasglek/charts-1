cd1f9bd4149aac131d60d6c263eca43ae2cd8808
Taras Glek <taras@purestorage.com>
2017-03-30 11:46:56 -0700

    misc

--- provision_pg.py
+++ provision_pg.py
@@ -19,20 +19,26 @@ def list_pg():
             continue
         yield (name, int(m.group(1)))
 
+def provision_one(port):
+    run("helm install --name p{port} postgresql-0.6.0.tgz --set persistence.storageClass=pure-provisioner,postgresPassword=taras,externalIP=10.19.66.145,port={port}".format(port=port))
+    
 def provision_another():
     new_port = 5432
     print sys.argv
     for (pg, port) in list_pg():
         new_port = max(new_port, port + 1)
-    run("helm install --name p{port} postgresql-0.6.0.tgz --set persistence.storageClass=pure-provisioner,postgresPassword=taras,externalIP=10.19.66.145,port={port}".format(port=new_port))
+    provision_one(new_port)
 
 def delete_all():
     for (pg, port) in list_pg():
         run("helm delete --purge " + pg)
 
 def main():
-    if sys.argv[1:] == ["--delete-all"]:
+    args = sys.argv[1:]
+    if args == ["--delete-all"]:
         delete_all()
+    elif len(args) == 2 and args[0] == "--port":
+        provision_one(int(args[1]))
     else:
         provision_another()
 
